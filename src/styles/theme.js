28261c614754073052c937c13f9d6c5c7e98b74b
kwonhygge <cleancode.kwon@gmail.com>
2023-06-23 09:54:16 +0900

    feat: boilerplate setting

---
+++ src/styles/theme.js
@@ -0,0 +1,21 @@
+const colors = {
+    brightGray: '#eee',
+    black: '#000',
+    whiteGray: '#f5f5f5',
+};
+
+const fontFamily = `-apple-system,
+    BlinkMacSystemFont, 'Apple SD Gothic Neo', 'Segoe UI', sans-serif,
+    'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'`;
+
+const fontSizes = {
+    size16: '1.6rem',
+};
+
+const theme = {
+    colors,
+    fontFamily,
+    fontSizes,
+};
+
+export default theme;
